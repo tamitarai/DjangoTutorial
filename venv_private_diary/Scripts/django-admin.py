3fb59c30c45927273f9ce2078696fce7c3ea9ea6
tamitarai <tamitarai@tech.softbank.co.jp>
2020-01-19 20:01:14 +0900

    first commit

---
+++ venv_private_diary/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tamitarai.intra\documents\00_workspace\books\djangodeveloptutorial\venv_private_diary\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
